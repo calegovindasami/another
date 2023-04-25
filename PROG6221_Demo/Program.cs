1e461d72fc73c66a243fc3fae3469296707d7d4d
lab_services_student <lab_services_student@lab000000>
2023-04-25 16:54:56 +0000

    Add project files.

---
+++ PROG6221_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG6221_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
